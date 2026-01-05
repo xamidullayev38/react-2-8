6b9075b9a414c4a76316c5b57d4bfb85f9389d82
xamidullayev38 <xamidullayevsaidkamol@gmail.com>
2026-01-05 13:17:15 +0500

    task-1 finished

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -5,23 +6,57 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
 export default function ButtonAppBar() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const open = Boolean(anchorEl);
+
+  const handleMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          {/* MOBILE MENU ICON */}
+          <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              onClick={handleMenuOpen}
+            >
+              <MenuIcon />
+            </IconButton>
+          </Box>
+
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>
             Femmecubator
           </Typography>
-          
-          <Button color="inherit">Listings</Button>
-          <Button color="inherit">Mentors</Button>
-          <Button color="inherit">My account</Button>
-          <Button color="inherit">Log Out</Button>
+
+          <Box sx={{ display: { xs: 'none', md: 'flex' }, gap: 1 }}>
+            <Button color="inherit">Listings</Button>
+            <Button color="inherit">Mentors</Button>
+            <Button color="inherit">My account</Button>
+            <Button color="inherit">Log Out</Button>
+          </Box>
         </Toolbar>
       </AppBar>
+
+      <Menu anchorEl={anchorEl} open={open} onClose={handleMenuClose}>
+        <MenuItem onClick={handleMenuClose}>Listings</MenuItem>
+        <MenuItem onClick={handleMenuClose}>Mentors</MenuItem>
+        <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+        <MenuItem onClick={handleMenuClose}>Log Out</MenuItem>
+      </Menu>
     </Box>
   );
 }
